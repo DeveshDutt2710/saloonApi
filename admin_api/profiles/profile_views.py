f75be0e1985ceba4cf85c26788d7159335943fd0
DeveshDutt2710 <dd123.ues2017@gmail.com>
2021-03-30 23:03:39 +0530

    abstract removed

--- admin_api/profiles/profile_views.py
+++ admin_api/profiles/profile_views.py
@@ -3,13 +3,16 @@ from rest_framework import status as status_codes
 from django.http import JsonResponse
 from .profile_service import Profiles
 from .profile_impl import ProfileImpl
-from .abstract import * 
+
 
 class AllProfileView(APIView):
 
     def get(self, request, *args, **kwargs):
-
-        response = allProfileView.getAllProfile(request)
+        query_params = request.query_params
+        page_no = query_params.get('page', 1)
+        page_size = query_params.get("page_size", 10)
+        profile_manager = ProfileImpl(page=page_no, page_size=page_size)
+        response = profile_manager.fetch_all_profiles()
 
         return JsonResponse(response, status=status_codes.HTTP_200_OK)
 
@@ -18,7 +21,8 @@ class GetProfileView(APIView):
 
     def get(self, request, profile_id, *args, **kwargs):
 
-        response = getProfileView.getProfileById(profile_id)
+        profile_manager = ProfileImpl(profile_id)
+        response = profile_manager.fetch_profile_by_id()
 
         return JsonResponse(response, status=status_codes.HTTP_200_OK)
 
@@ -26,8 +30,8 @@ class GetProfileView(APIView):
 class CreateProfileView(APIView):
 
     def post(self, request, *args, **kwargs):
-
-        response = createProfileView.createProfile(request)
+        profile_manager = ProfileImpl()
+        response = profile_manager.create_profile(request.data)
 
         return JsonResponse(response, status=status_codes.HTTP_200_OK)
 
@@ -35,8 +39,8 @@ class CreateProfileView(APIView):
 class UpdateProfileView(APIView):
 
     def post(self, request, profile_id, *args, **kwargs):
-
-        response = updateProfileView.updateProfile(request, profile_id)
+        profile_manager = ProfileImpl(profile_id)
+        response = profile_manager.update_profile(request.data)
 
         return JsonResponse(response, status=status_codes.HTTP_200_OK)
 
@@ -45,7 +49,8 @@ class DeleteProfileView(APIView):
 
     def post(self, request, profile_id, *args, **kwargs):
 
-        response = deleteProfileView.getAllProfile(profile_id)
+        profile_manager = ProfileImpl(profile_id)
+        response = profile_manager.delete_profile()
 
         return JsonResponse(response, status=status_codes.HTTP_200_OK)
 
@@ -53,6 +58,12 @@ class DeleteProfileView(APIView):
 class SearchProfileView(APIView):
 
     def post(self, request, query, *args, **kwargs):
-        response = searchProfileView.getAllProfile(request, query)
+
+        query_params = request.query_params
+        page_no = query_params.get('page', 1)
+        page_size = query_params.get("page_size", 10)
+
+        profile_manager = ProfileImpl(page=page_no, page_size=page_size)
+        response = profile_manager.search_profile(query)
 
         return JsonResponse(response, status=status_codes.HTTP_200_OK)
